3a584fd85af5d08cd5a3dd865ad5405576f7f34e
Omar <omarelias1997@gmail.com>
2019-05-29 21:17:37 -0400

    Seed heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 10.times do |i|
-#     Product.create(name: Faker::Commerce.product_name,
-#                    price: Faker::Commerce.price.to_i,
-#                    description: Faker::Quote.famous_last_words,
-#                    photo:"https://picsum.photos/id/#{i}/200/300")
-# end
-#
-# 10.times do |i|
-#     Information.create(title: Faker::Commerce.product_name,
-#                    description: Faker::Quote.famous_last_words,)
-#
-#
-# end
+10.times do |i|
+    Product.create(name: Faker::Commerce.product_name,
+                   price: Faker::Commerce.price.to_i,
+                   description: Faker::Quote.famous_last_words,
+                   photo:"https://picsum.photos/id/#{i}/200/300")
+end
 
-# 20.times do |i|
-#   start_at = Random.rand(10).days.ago + 5.days
-#   Tested.create!(title: Faker::Name.name, start: start_at)
-# end
+10.times do |i|
+    Information.create(title: Faker::Commerce.product_name,
+                   description: Faker::Quote.famous_last_words,)
+
+
+end
+
+20.times do |i|
+  start_at = Random.rand(10).days.ago + 5.days
+  Tested.create!(title: Faker::Name.name, start: start_at)
+end
